38b2601884df26c8c5d9387c43763a4aab81a08b
Guijie Lee <landriesnidis@yeah.net>
2018-09-18 21:00:39 +0800

    回文文本判断练习

---
+++ Lesson2_Palindrome/Lesson2_Palindrome/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2_Palindrome
+{
+    class Program
+    {
+        static Boolean isPalindrome(string s)
+        {
+            Boolean ret = true;
+            char[] array = s.ToArray();
+            for (int i = 0; i<array.Length;i++)
+            {
+                if (array[i] != array[array.Length - i - 1])
+                {
+                    ret = false;
+                    break;
+                }
+            }
+            return ret;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入数值：");
+            String str = Console.ReadLine();
+            Console.WriteLine(isPalindrome(str));
+
+            Console.ReadLine();
+
+        }
+    }
+}
