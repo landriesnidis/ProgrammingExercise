95ba9fa5d4fea47d4e388072f59fc18e0c256ea1
Guijie Lee <landriesnidis@yeah.net>
2018-10-02 04:18:14 +0800

    优化算法

--- Calculate/src/pers/landriesnidis/calculate/Calculate.java
+++ Calculate/src/pers/landriesnidis/calculate/Calculate.java
@@ -1,6 +1,7 @@
 package pers.landriesnidis.calculate;
 
 import java.util.LinkedList;
+import java.util.Stack;
 
 import pers.landriesnidis.calculate.exception.InvalidExpressionException;
 import pers.landriesnidis.calculate.exception.NumberFormatException;
@@ -10,7 +11,7 @@ public class Calculate {
 	
 	public static void main(String[] args) throws NumberFormatException, InvalidExpressionException, UnknownOperatorException {
 		
-		args = new String[]{"3.14*5*5-5*-3"};
+		if(args.length==0) args = new String[]{"2*3.14*(-2+10)"};
 		
 		if(args.length>0){
 			System.out.println(operation(args[0]));
@@ -20,19 +21,18 @@ public class Calculate {
 	}
 	
 	/**
-	 * 表达式预处理
+	 * 表达式补全
 	 * 由于“+”、“-”可作为单目表达式使用（如：-3+2），因此通过预处理为表达式在不影响结果的前提下添加0，使其保证能有二元进行运算
 	 * 示例：
 	 * 8*-1.25+12 => 8*(0-1.25)+12
 	 * @param expression 待处理的表达式
 	 * @return 处理后的表达式
 	 */
-	private static String expressionPretreatment(String expression){
+	private static String expressionCompletion(String expression){
 		StringBuilder sb = new StringBuilder(expression);
-		int zero = 0;
 		int len = sb.length();
 		boolean b = false;
-		for(int i=0;i<len+zero;++i){
+		for(int i=0;i<len;++i){
 			char c = sb.charAt(i);
 			b = false;
 			
@@ -47,11 +47,11 @@ public class Calculate {
 					}
 				}
 				if(b){
-					zero+=2;
-					for(int j=i+2;i<len+zero;++j){
+					len+=2;
+					for(int j=i+2;i<len;++j){
 						if(RPN.isOperator(sb.charAt(j))){
 							sb.insert(j, ')');
-							++zero;
+							++len;
 							break;
 						}
 					}
@@ -63,8 +63,8 @@ public class Calculate {
 	}
 	
 	/**
-	 * 进行计算
-	 * 算法需要优化(类型转换消耗频繁)
+	 * 表达式计算
+	 * 先将中缀表达式转换为后缀表达式，再计算表达式的结果
 	 * @param expression 表达式
 	 * @return 运算结果
 	 * @throws NumberFormatException 数值非浮点型
@@ -74,47 +74,48 @@ public class Calculate {
 	public static double operation(String expression) throws NumberFormatException, InvalidExpressionException, UnknownOperatorException{
 		
 		// 预处理
-		expression = expressionPretreatment(expression);
+		expression = expressionCompletion(expression);
 		
 		// 使用逆波兰算法处理
 		LinkedList<String> rpnList = RPN.parse(expression);
-		// System.out.println(rpnList);
+		
+		// 保存每一步运算结果的操作数栈
+		Stack<Double> operands = new Stack<Double>();
 		
 		// 若表达式第一位为运算符，则表达式无效
 		if(RPN.isOperator(rpnList.get(0).charAt(0))) throw new InvalidExpressionException(expression);
 		
-		for(int i=1;i<rpnList.size();++i){
-			String elem = rpnList.get(i);
+		// 遍历逆波兰表达式中每一项元素
+		for(String elem:rpnList){
 			
 			// 若是运算符
 			if(RPN.isOperator(elem.charAt(0))){
-				String value1,value2;
 				
-				if(i-2<0) throw new InvalidExpressionException(expression);
+				// 不满足二元运算抛出异常
+				if(operands.size()<2) throw new InvalidExpressionException(expression);
 				
-				value1 = rpnList.get(i-2);
-				value2 = rpnList.get(i-1);
-				
-				// 检查数值有效性
-				if(!RegexUtils.isValue(value1)) throw new NumberFormatException(value1);
-				if(!RegexUtils.isValue(value2)) throw new NumberFormatException(value2);
+				// 从操作数栈取出栈顶的两个操作数
+				double value2 = operands.pop();
+				double value1 = operands.pop();
 				
 				// 获得运算结果
-				String result = String.valueOf(binaryOperation(elem.charAt(0),Double.parseDouble(value1),Double.parseDouble(value2)));
+				double result = binaryOperation(elem.charAt(0),value1,value2);
 				
-				// 将消耗掉的运算符和数值删除替换成运算结果
-				rpnList.remove(i);
-				rpnList.remove(i-1);
-				rpnList.remove(i-2);
-				rpnList.add(i-2,result);
-				i-=2;
+				// 将计算结果压栈
+				operands.push(result);
 				
-				// System.out.println(rpnList);
-				continue;
+			// 如果是数值
+			}else{
+				// 检查数值有效性
+				if(!RegexUtils.isValue(elem)) throw new NumberFormatException(elem);
+				operands.push(Double.parseDouble(elem));
 			}
 		}
+		// 如果操作数栈中元素个数不唯一则说明表达式不正确
+		if(operands.size()!=1) throw new InvalidExpressionException(expression);
 		
-		return Double.parseDouble(rpnList.get(0));
+		// 返回操作数栈中唯一的元素
+		return operands.pop();
 	}
 	
 	/**
